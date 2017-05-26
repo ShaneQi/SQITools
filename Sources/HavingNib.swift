e51446e664810567006185d957dde4fef05a15fa
Shane Qi <qizengtai@gmail.com>
2017-05-26 12:08:07 -0500

    Added HavingNib protocol.

---
+++ Sources/HavingNib.swift
@@ -0,0 +1,23 @@
+//
+//  HavingNib.swift
+//  SQITools
+//
+//  Created by Shane Qi on 5/26/17.
+//  Copyright © 2017 Shane Qi. All rights reserved.
+//
+
+import UIKit
+
+public protocol HavingNib: Identifiable {
+
+	static var nibName: String { get }
+	static var nib: UINib { get }
+
+}
+
+public extension HavingNib {
+
+	static var nibName: String { return identifier }
+	static var nib: UINib { return UINib(nibName: nibName, bundle: nil) }
+
+}
